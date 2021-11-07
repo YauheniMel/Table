1c0542198f800005ed2e1db7d7b483937de680f6
YauheniMel <207melnik@gmail.com>
2021-11-07 21:48:42 +0300

    feat: add head and footer for table

--- table/src/App.js
+++ table/src/App.js
@@ -1,18 +1,29 @@
-import './App.css';
-import TemplateRegulator from './components/atoms/TemplateRegulator/TemplateRegulator';
+import './App.scss';
 import Pagination from './components/molecules/Pagination/Pagination';
+import TableHead from './components/molecules/TableHead/TableHead';
 import TemplateLineTable from './components/molecules/TemplateLineTable/TemplateLineTable';
 
 function App() {
   return (
     <div className="App">
       <table>
+        <thead>
+          <TableHead />
+        </thead>
         <tbody>
           <TemplateLineTable />
+          <TemplateLineTable />
+          <TemplateLineTable />
+          <TemplateLineTable />
         </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="7">
+              <Pagination />
+            </td>
+          </tr>
+        </tfoot>
       </table>
-      <Pagination />
-      <TemplateRegulator title="Location" />
     </div>
   );
 }
---
+++ table/src/components/molecules/TableHead/TableHead.jsx
@@ -0,0 +1,42 @@
+import Checkbox from '../../atoms/Checkbox/Checkbox';
+import TemplateRegulator from '../../atoms/TemplateRegulator/TemplateRegulator';
+import styles from './TableHead.module.scss';
+
+export default function TableHead() {
+
+  return (
+    <tr className={styles.wrapper}>
+      <th>
+        <Checkbox />
+      </th>
+      <th>
+        <TemplateRegulator
+          title="Band"
+        />
+      </th>
+      <th>
+        <TemplateRegulator
+          title="Song"
+        />
+      </th>
+      <th>
+        <TemplateRegulator
+          title="Date"
+        />
+      </th>
+      <th>
+        <TemplateRegulator
+          title="Evaluation"
+        />
+      </th>
+      <th>
+        <TemplateRegulator
+          title="Location"
+        />
+      </th>
+      <th>
+        Action
+      </th>
+    </tr>
+  );
+}
