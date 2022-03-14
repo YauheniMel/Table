bf59a6253898ae4d30859e5820c768f3b4e1cec1
YauheniMel <207melnik@gmail.com>
2022-03-14 16:43:05 +0300

    refactor: Update style

--- src/components/layout/DetailsPageLayout/DetailsPageLayout.jsx
+++ src/components/layout/DetailsPageLayout/DetailsPageLayout.jsx
@@ -1,4 +1,5 @@
 import Loader from '../../atoms/Loader/Loader';
+import styles from './DetailsPageLayout.module.scss';
 
 export default function DetailsPageLayout({
   band,
@@ -9,13 +10,15 @@ export default function DetailsPageLayout({
   isLoading,
 }) {
   return (
-    <div>
+    <div className={styles.wrapper}>
       {isLoading && <Loader />}
-      <h3>{band}</h3>
-      <strong>{song}</strong>
-      <time>{date}</time>
-      <p>{comment}</p>
+      <div className={styles.head}>
+        <h3 className={styles.band}>{band}</h3>
+        <strong>{song}</strong>
+        <time>{date}</time>
+      </div>
       <img src={window.location.origin + '/photo/' + photoName} alt="photo" />
+      <p>{comment}</p>
     </div>
   );
 }
